95bab4c4c615a2a9277f860cc810776dd9ab7a1a
Austin Jones <austin.jones231@gmail.com>
2021-02-03 01:33:19 -0500

    Added opportunity checker and logging

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import asyncio
 import argparse
-import json
+import logging
 
 import yaml
 from trader.trader import Trader
 
 
 async def main():
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', filename='trader.log', encoding='utf-8', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', default='config.yml', help='YAML file for configuration information')
     args = parser.parse_args()
@@ -15,7 +16,11 @@ async def main():
         config = yaml.load(file, Loader=yaml.FullLoader)
 
     t = Trader(config)
-    print(json.dumps(await t.get_markets()))
+    await t.prepare()
+    while True:
+        logging.info('Checking:')
+        cr1, cr2 = await t.find_opportunities()
+        logging.info(f'Path A: {cr1} Path B: {cr2}')
 
 if __name__ == "__main__":
     asyncio.run(main())
--- trader/trader.py
+++ trader/trader.py
@@ -1,13 +1,21 @@
+import copy
+
 import ccxt.async_support as ccxt
 
 
 class Trader:
+
+    markets = {}
+    symbols = []
+    symbols_str = []
 
     def __init__(self, args):
         if 'timeout' not in args:
             args['timeout'] = 30000
         if 'enableRateLimit' not in args:
             args['enableRateLimit'] = True
+        if 'symbols' not in args or not args['symbols']:
+            args['symbols'] = ['BTC/USDT', 'LTC/USDT']
 
         self.exchange_id = args['exchange']
         exchange_class = getattr(ccxt, self.exchange_id)
@@ -17,8 +25,31 @@ class Trader:
             'timeout': args['timeout'],
             'enableRateLimit': args['enableRateLimit']
         })
+        self.symbols_str = args['symbols']
 
-    async def get_markets(self):
-        m = await self.exchange.load_markets()
+    async def prepare(self):
+        self.markets = await self.exchange.load_markets()
+        for s in self.symbols_str:
+            self.symbols.append(self.exchange.markets[s])
+        await self.close()
+
+    async def close(self):
         await self.exchange.close()
-        return m
+
+    async def find_opportunities(self):
+        symbols = copy.deepcopy(self.symbols)
+        for s in symbols:
+            orderbook = await self.exchange.fetch_order_book(s['symbol'])
+            s['bid'] = orderbook['bids'][0][0] if len(orderbook['bids']) > 0 else None
+            s['ask'] = orderbook['asks'][0][0] if len(orderbook['asks']) > 0 else None
+            s['spread'] = (s['ask'] - s['bid']) if (s['bid'] and s['ask']) else None
+
+        # Buy - Buy - Sell
+        cr1 = (1 / symbols[0]['ask']) * (1 / symbols[1]['ask']) * symbols[2]['bid']
+
+        # Buy - Sell - Sell
+        cr2 = (1 / symbols[2]['ask']) * (symbols[1]['bid']) * symbols[0]['bid']
+
+        await self.close()
+        return cr1, cr2
+
